4ef071c1f324a959381c330986c833ace95b78f1
Parada <paradafaii28@gmail.com>
2020-10-06 13:12:37 -0600

    fetch medium api

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Blogs.css'
 
 export default class Blogs extends Component {
     state ={
@@ -8,13 +9,14 @@ export default class Blogs extends Component {
     componentDidMount() {
         fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@alicerichardson28')
          .then(res => res.json())
-         .then(blogs => this.setState({blogs}))
+         .then(res => res.items)
+         .then(res => this.setState({blogs: res}))
     }
 
     showBlogs = this.state.blogs.map(blog => {
         return (
-            <ul>
-                <li>{blog.content}</li>
+            <ul className='blogs-container'>
+                <li className='blogs-card'><h1>{blog.items.content}</h1></li>
             </ul>
         )
     })
