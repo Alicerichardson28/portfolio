10728f1d8eb595052a772dda438d1693406f7781
Parada <paradafaii28@gmail.com>
2020-10-06 12:55:22 -0600

    add medium api to blogs pages

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 
 export default class Blogs extends Component {
+    state ={
+        blogs: []
+    }
+    
+    componentDidMount() {
+        fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@alicerichardson28')
+         .then(res => res.json())
+         .then(blogs => this.setState({blogs}))
+    }
+
+    showBlogs = this.state.blogs.map(blog => {
+        return (
+            <ul>
+                <li>{blog.content}</li>
+            </ul>
+        )
+    })
     render() {
+
         return (
             <div>
-                <h1>Hello</h1>
+                {this.showBlogs}
             </div>
         )
     }
