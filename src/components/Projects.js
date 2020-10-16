a226b07a9d376ff8fd29a35c1389849f869423f9
Parada <paradafaii28@gmail.com>
2020-10-16 15:24:35 -0600

    style project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,19 +7,22 @@ export default class Projects extends Component {
     render() {
         return (
             <div>
-                <Link to='/'><h2 className='logo'>A</h2></Link>
-                <ul className='nav-container'>
-                    <Link to="/about">
-                        <li className='nav-list'>About</li>
-                    </Link>
-                    <Link to="/blogs">
-                        <li className='nav-list'>Blogs</li>
-                    </Link>
-                    <Link to="/projects">
-                        <li className='nav-list'>Projects</li>
-                    </Link>
-                </ul> 
+                <div className='navbar-bg'>
+                    <Link to='/'><h2 className='logo'>A</h2></Link>
+                    <ul className='nav-container'>
+                        <Link to="/about">
+                            <li className='nav-list'>About</li>
+                        </Link>
+                        <Link to="/blogs">
+                            <li className='nav-list'>Blogs</li>
+                        </Link>
+                        <Link to="/projects">
+                            <li className='nav-list'>Projects</li>
+                        </Link>
+                    </ul> 
+                </div>
                 <div>
+                    <h1>some of my latest work</h1>
                     <ul className='project-container'>
                         <li className='project-card'>
                             <a href='http://ec2-3-128-181-53.us-east-2.compute.amazonaws.com/'>
