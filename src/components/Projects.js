1f41764e8711ef7d1131991e779326858f077667
Parada <paradafaii28@gmail.com>
2020-10-16 15:05:04 -0600

    add project to the projects page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+import './Projects.css'
+
 
 export default class Projects extends Component {
     render() {
         return (
             <div>
-                <h1>Projects</h1>
+                <Link to='/'><h2 className='logo'>A</h2></Link>
+                <ul className='nav-container'>
+                    <Link to="/about">
+                        <li className='nav-list'>About</li>
+                    </Link>
+                    <Link to="/blogs">
+                        <li className='nav-list'>Blogs</li>
+                    </Link>
+                    <Link to="/projects">
+                        <li className='nav-list'>Projects</li>
+                    </Link>
+                </ul> 
+                <div>
+                    <ul className='project-container'>
+                        <li className='project-card'>
+                            <a href='http://ec2-3-128-181-53.us-east-2.compute.amazonaws.com/'>
+                                <h1 className='project-name'>Ecofreak</h1>
+                                <img className='eco-freak-project' src='https://cdn3.f-cdn.com//files/download/110732690/plant.png'/>
+                                <h1 className='font-stack'>Tech Stack // MEVN app deploy with AWS EC2</h1>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
