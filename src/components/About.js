3d4ff4896233c6d2bb27d5e3ceb8e216d49bba63
Parada <paradafaii28@gmail.com>
2020-10-20 11:19:44 -0600

    add content to about pages

--- src/components/About.js
+++ src/components/About.js
@@ -29,12 +29,19 @@ export default class About extends Component {
                         <div className='content'>
                             <h1 className='about'>About</h1>
                             <p className='content-about'>
-                                I am a full stack developer with a background in interior design. <br></br>
-                                I am passionate about coding, and I'm excited to continue growing my skills.<br></br>
+                                I am a <u>full stack developer </u> with a background in interior design. I am <br></br>
+                                passionate about coding, and I'm excited to continue growing my skills.<br></br>
                                 <br></br>
-                                I am looking for a full-time position as a Software Engineer. I have experience in Javascript,<br></br>
-                                Ruby on Rails, React, React Native, HTML, CSS, and I am also open to learning any new technical <br></br> 
-                                language as well.I am hardworking, a quick learner, and a great team player. 
+                                <br></br>
+                                I am looking for a full-time position as a Software Engineer. I have <br></br> 
+                                experience in <u>ruby on rails, javascript, react, git/github, JWT, SQL </u> <br></br> 
+                                <br></br>
+                                <br></br>
+                                I have also done my best to build on that foundation, by continuing to push myself to to learn even more languages, such as <u>Vue.js, MongoDB, Node.js, and AWS.</u>
+                                and I am also open to learning any new technical language as well. <br></br>
+                                <br></br>
+                                <br></br>
+                                I am personable with a talent for developing solid relationships with co-workers and clients. My work ethic and dedication to exceeding expectations is what keeps me motivated to impress, and I continuously strive to improve my craft. 
                             </p>
                         </div>
                 </div>
