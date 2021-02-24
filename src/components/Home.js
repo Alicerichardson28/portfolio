b7fae35ba64c9930a33015bbd42b0ce536b8bb77
Parada <paradafaii28@gmail.com>
2021-02-24 13:06:52 -0700

    change style tech container

--- src/components/Home.js
+++ src/components/Home.js
@@ -84,56 +84,60 @@ export default class Navbar extends Component {
                 </div>
                 <div id='tech-tools-container'>
                     <h1 className="tech-title">Tools & Technologies</h1>
-                        <ul className="lists-skill">
-                            <li className="card">
-                                <img src={javascript} className="svg"/>
-                                <h1 className="tech-name">Javascript</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Html} className="svg"/>
-                                <h1 className="tech-name">Html</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Css} className="svg"/>
-                                <h1 className="tech-name">Css</h1>
-                            </li>
-                            <li className="card">
-                                <img src={ReactIcon} className="svg"/>
-                                <h1 className="tech-name">React.js</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Npm} className="svg"/>
-                                <h1 className="tech-name">NPM</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Bootstrap} className="svg"/>
-                                <h1 className="tech-name">Bootstrap</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Git} className="svg"/>
-                                <h1 className="tech-name">Git</h1>
-                            </li>
-                            <li className="card"> 
-                                <img src={GitHub} className="svg"/>
-                                <h1 className="tech-name">GitHub</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Heroku} className="svg"/>
-                                <h1 className="tech-name">Heroku</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Vscode} className="svg"/>
-                                <h1 className="tech-name">Vscode</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Slack} className="svg"/>
-                                <h1 className="tech-name">Slack</h1>
-                            </li>
-                            <li className="card">
-                                <img src={Node} className="svg"/>
-                                <h1 className="tech-name">Node.js</h1>
-                            </li>
-                        </ul>
+                        {/* <div className="skills-container-outside"> */}
+                            {/* <div className="skills-container"> */}
+                                <ul className="lists-skill">
+                                    <li className="card">
+                                        <img src={javascript} className="svg"/>
+                                        <h1 className="tech-name">Javascript</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Html} className="svg"/>
+                                        <h1 className="tech-name">Html</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Css} className="svg"/>
+                                        <h1 className="tech-name">Css</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={ReactIcon} className="svg"/>
+                                        <h1 className="tech-name">React.js</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Npm} className="svg"/>
+                                        <h1 className="tech-name">NPM</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Bootstrap} className="svg"/>
+                                        <h1 className="tech-name">Bootstrap</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Git} className="svg"/>
+                                        <h1 className="tech-name">Git</h1>
+                                    </li>
+                                    <li className="card"> 
+                                        <img src={GitHub} className="svg"/>
+                                        <h1 className="tech-name">GitHub</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Heroku} className="svg"/>
+                                        <h1 className="tech-name">Heroku</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Vscode} className="svg"/>
+                                        <h1 className="tech-name">Vscode</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Slack} className="svg"/>
+                                        <h1 className="tech-name">Slack</h1>
+                                    </li>
+                                    <li className="card">
+                                        <img src={Node} className="svg"/>
+                                        <h1 className="tech-name">Node.js</h1>
+                                    </li>
+                                </ul>
+                            {/* </div> */}
+                        {/* </div> */}
                     </div>
                 <div id='blog-post'>
                     <h1>Blogs posts</h1>
