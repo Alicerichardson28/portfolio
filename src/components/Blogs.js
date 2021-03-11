682f2892326cda6bf09ae147f703cde9c7e9d02b
Parada <paradafaii28@gmail.com>
2021-03-11 13:18:19 -0700

    styles blogs container by using grid

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -18,24 +18,22 @@ export default class Blogs extends Component {
              {
             return ( 
                 <div className='blog-container'>
-                    <ul className='blogs-post-container'>
-                        <li className='blogs-card' key={index}>
+                        <div className='blogs-card' key={index}>
                             <a href={blog.link} target="_blank" rel="noopener noreferrer">
                                 <h1 className='title'>{blog.title}</h1>
                             </a>
-                        </li>
-                    </ul>
+                        </div>
                 </div>
             )
         })
         
         return (
-            <>
-                <h1 className='blog-title'>Some of my blogs post</h1>
-                <div>
-                    {showBlogs}
-                </div>
-            </>
+        <>
+            <h1 className='blog-title'>Some of my blogs post</h1>
+            <div className='big-blogs-container'>
+                {showBlogs}
+            </div>
+        </>
         )
     }
 }
