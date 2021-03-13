067388776f4a6180a7f8d555850b9b70bed576f4
Parada <paradafaii28@gmail.com>
2021-03-13 13:40:30 -0700

    add btn-hamburger to hom page and also make responsive, create open menu function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import PDF from '../picture/Alice Richardson Resume.pdf'
 import './Home.css'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -12,59 +12,69 @@ import Footer from './Footer'
 import BlogsAtHomePage from './BlogsAtHomePage.js'
 
 library.add(fab)
-export default class Navbar extends Component {
-    render() {
-        return (
-            <>
-                <div id='nav-container' className='section-container'>
-                    {/* <div id='nav-content-container' className='content-container'> */}
-                    <div id='nav-content-container'>
-                        <a href="#about-section-container" className='nav-list-home'>
-                            About
-                        </a>
-                        <a href="#tech-tools-container" className='nav-list-home'>
-                            Tools & Tech
-                        </a>
-                        <a href="#project-container" className='nav-list-home'>
-                            My works
-                        </a>
-                        <a href="#blog-post" className='nav-list-home'>
-                            Blogs
-                        </a>
-                    </div>
-                </div> 
-                <div id='first-container' className='section-container'>
-                        <div className="name-photo-container content-container">
-                            <div className='profile-container'>
-                                <Icon/>
-                                <br></br>
-                                <h1 className='name'>Alice Richardson</h1>
-                                <h2 className='job-title'>Frontend & Full stack developer</h2>
-                                <h3 className='job-title-h3'>UX & UI Designer</h3>
-                                <br></br>
-                                <button className='button'><a href={PDF} target="_blank" rel="noopener noreferrer">Download Resume </a></button>
-                            </div>
-                            <div className='photo-container'>
-                                <img alt="alice picture" className='alice1-photo' src={alice1}/>
-                            </div>
+function Navbar() {
+    const [showMenu, setShowMenu] = useState(false)
+    return (
+        <>
+            <div id='nav-container' className='section-container'>
+                {/* <div id='nav-content-container' className='content-container'> */}
+                <div id='nav-content-container'>
+                    <a href="#about-section-container" className='nav-list-home'>
+                        About
+                    </a>
+                    <a href="#tech-tools-container" className='nav-list-home'>
+                        Tools & Tech
+                    </a>
+                    <a href="#project-container" className='nav-list-home'>
+                        My works
+                    </a>
+                    <a href="#blog-post" className='nav-list-home'>
+                        Blogs
+                    </a>
+                </div>
+                <div className={`menu-btn ${showMenu ? "open":""}`} onClick={() => setShowMenu(!showMenu)}>
+                    <div className="menu-btn__burger"></div>
+                </div>
+            </div> 
+            <div className = {`nav-sidebar ${showMenu ? "open" : "close"}`}>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/Menu">Menu</a></li>
+                        <li><a href="/Contact">Contact</a></li>
+                    </ul>
+                </div>
+            <div id='first-container' className='section-container'>
+                    <div className="name-photo-container content-container">
+                        <div className='profile-container'>
+                            <Icon/>
+                            <br></br>
+                            <h1 className='name'>Alice Richardson</h1>
+                            <h2 className='job-title'>Frontend & Full stack developer</h2>
+                            <h3 className='job-title-h3'>UX & UI Designer</h3>
+                            <br></br>
+                            <button className='button'><a href={PDF} target="_blank" rel="noopener noreferrer">Download Resume </a></button>
                         </div>
-                </div>
-                <div id='about-section-container' className='section-container'>
-                    <About/>
-                </div>
-                <div id='tech-tools-container' className='section-container'>
-                    <TechSkill/>
-                </div>
-                <div id='project-container' className='section-container'>
-                    <ProjectContainer/>
-                </div>
-                <div id='blog-post' className='section-container'>
-                    <BlogsAtHomePage />
-                </div>
-                <div className='section-container'>
-                    <Footer />
-                </div>
-            </>
-        )
-    }
+                        <div className='photo-container'>
+                            <img alt="alice picture" className='alice1-photo' src={alice1}/>
+                        </div>
+                    </div>
+            </div>
+            <div id='about-section-container' className='section-container'>
+                <About/>
+            </div>
+            <div id='tech-tools-container' className='section-container'>
+                <TechSkill/>
+            </div>
+            <div id='project-container' className='section-container'>
+                <ProjectContainer/>
+            </div>
+            <div id='blog-post' className='section-container'>
+                <BlogsAtHomePage />
+            </div>
+            <div className='section-container'>
+                <Footer />
+            </div>
+        </>
+    )
 }
+export default Navbar;
