efa7c9715ca9a51920309e62047c48aa5c769d55
Parada <paradafaii28@gmail.com>
2021-03-31 10:45:28 -0600

    project js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,15 +2,11 @@ import React, { Component } from 'react'
 import './Projects.css'
 import thaiCardProject from '../picture/thai-card.gif'
 import emperor from '../picture/emperor.gif'
+import parkingSpotFinder from '../picture/Parking-spot-finder.gif'
+import coffee from '../picture/coffee-shop.gif'
+import ecoFreak from '../picture/Eco-freak.gif'
 import { BiWorld } from "react-icons/bi";
 import { AiFillGithub } from "react-icons/ai";
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-
-
 
 export default class Projects extends Component {
     render() {
@@ -30,27 +26,23 @@ export default class Projects extends Component {
                         </li>
                         <li className='project-card'>
                                 <div className='video-container'>
-                                    <iframe title="EcoFreak" className='video' 
-                                            src="https://www.youtube.com/embed/wtGFgzJXCxw">
-                                    </iframe>
+                                    <img alt='eco-freak-gif' className='video' src={ecoFreak}/>
                                 </div>
                                 <div className='project-content'>
                                     <div className='icon-container'>
                                         <a href="https://github.com/Alicerichardson28/eco-freak-frontend-with-Vue" target="_blank" rel="noopener noreferrer">
                                             <AiFillGithub size='27px' color='black'/>
                                         </a>
-                                        <BiWorld size='27px'/>
+                                        {/* <BiWorld size='27px'/> */}
                                     </div>
                                     <h1 className='project-name'>Eco-freak is a website to educate and show what we can do to save this world and help be more green.</h1>
-                                    <h1 className='project-name'><u>Tech Stack:</u> MEVN app
+                                    <h1 className='project-name'><u>Tech Stack:</u> MEVN app (Node express, Vue, MongoDB)
                                     </h1>
                                 </div>
                         </li>
                         <li className='project-card'>
                                 <div className='video-container'>
-                                    <iframe title="Parking spot finder" className='video' 
-                                            src="https://www.youtube.com/embed/QVFtjzVuoXc">
-                                    </iframe>
+                                    <img alt='parking-spot-finder-gif' className='video' id='parking-spot-gif' src={parkingSpotFinder}/>
                                 </div>
                                 <div className='project-content'>
                                     <div className='icon-container'>
@@ -75,8 +67,8 @@ export default class Projects extends Component {
                                             <BiWorld size='27px' color='black'/>  
                                         </a>
                                     </div>
-                                    <h1 className='project-name'>Parking spot finder was built to find a parking spot quickly and easily.</h1>
-                                    <h1 className='project-name'><u>Tech Stack:</u> Ruby on rails, React Native(expo), Authentication (JWT)</h1>
+                                    <h1 className='project-name'>Emperor is frontend website about furniture shop</h1>
+                                    <h1 className='project-name'><u>Tech Stack:</u> React, CI/CD, Docker, React testing </h1>
                                 </div>
                         </li>     
                         <li className='project-card'>
@@ -93,6 +85,23 @@ export default class Projects extends Component {
                                         </a>
                                     </div>
                                     <h1 className='project-name'>Web application and mix-and-match game for Thai language learning</h1>
+                                    <p className='project-name'><u>Tech Stack:</u> Ruby on Rails, Javascript, HTML, CSS </p>
+                                </div>
+                        </li>     
+                        <li className='project-card'>
+                                <div className='video-container'>
+                                    <img alt='coffee-shop-gif' className='video' src={coffee}/>
+                                </div>
+                                <div className='project-content'>
+                                    <div className='icon-container'>
+                                        <a href="https://github.com/Alicerichardson28/coffee-frontend-1" target="_blank" rel="noopener noreferrer">
+                                            <AiFillGithub size='27px' color='black'/>
+                                        </a>
+                                        {/* <a href="https://thai-alphabet-flashcard.web.app/" target="_blank" rel="noopener noreferrer">
+                                            <BiWorld size='27px' color='black'/>  
+                                        </a> */}
+                                    </div>
+                                    <h1 className='project-name'>coffee shop was design for create a system for ordering & tracking inventory.</h1>
                                     <p className='project-name'><u>Tech Stack:</u> Ruby on Rails, Javascript, HTML, CSS </p>
                                 </div>
                         </li>     
