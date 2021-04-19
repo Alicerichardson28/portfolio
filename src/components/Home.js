3fede468d721ac3264c0ff61c868e94a9c2cec73
Parada <paradafaii28@gmail.com>
2021-04-19 08:08:55 -0400

    change button to use just a tag then style a tag to look like a button

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,16 +43,16 @@ function Navbar() {
                             <p className='job-title'>Frontend & Full stack developer</p>
                             <p className='job-title-h3'>UX & UI Designer</p>
                             <br></br>
-                            <button 
+                            {/* <button 
                                 aria-expanded="false"
-                                className='button'>
+                                className='button'> */}
                                     <a 
                                     href={resume} 
                                     target="_blank" 
                                     rel="noopener noreferrer" 
                                     className='downloadResume'>View Resume 
                                     </a>
-                            </button>
+                            {/* </button> */}
                         </div>
                         <div className='photo-container'>
                             <img alt="first alice standing" className='alice1-photo' src={alice1}/>
