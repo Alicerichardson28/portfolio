f3ae5fb61164e1cda69df7faec527dcebb152679
Parada <paradafaii28@gmail.com>
2021-08-10 16:30:33 -0400

    adding react-bootstrap and update blog style

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Button, Card, Container } from 'react-bootstrap'
 import './Blogs.css'
 
 export default class Blogs extends Component {
     state ={
         blogs: []
     }
+   
     
     componentDidMount() {
         fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@alicerichardson28')
@@ -12,28 +14,34 @@ export default class Blogs extends Component {
          .then(res => res.items)
          .then(res => this.setState({blogs: res}))
     }
-
+    
     render() {
-        const showBlogs = this.state.blogs.map((blog, index) => 
-             {
+        console.log(this.state.blog)
+        const showBlogs = this.state.blogs.map((blog) => 
+        {
             return ( 
-                <div className='blog-container'>
-                        <div className='blogs-card' key={index}>
-                            <a href={blog.link} target="_blank" rel="noopener noreferrer">
-                                <h1 className='title'>{blog.title}</h1>
-                            </a>
-                        </div>
-                </div>
+                <Card className="card" key={blog.id}>
+                    <Card.Img className="blog-img" variant="top" src={blog.thumbnail} />
+                    <Card.Body>
+                        <Card.Title>{blog.title}</Card.Title>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary" href={blog.link} target="_blank">Read More</Button>
+                    </Card.Body>
+                </Card>   
             )
         })
         
         return (
         <>   
             <h1 className='blog-title'>Some of my blogs post</h1>
-            <div className='big-blogs-container'>
+            <Container fluid className="container-blogs">
                 {showBlogs}
-            </div>
+            </Container>
         </>
         )
     }
 }
+
