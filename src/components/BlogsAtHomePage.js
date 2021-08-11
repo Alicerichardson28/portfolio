77dfdd22093b26a261de0db6f3fe266e45c627c2
Parada <paradafaii28@gmail.com>
2021-08-11 10:10:01 -0400

    update

--- src/components/BlogsAtHomePage.js
+++ src/components/BlogsAtHomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './BlogsAtHomePage.css'
+import { Button, Card, Container } from 'react-bootstrap'
 import WomenWhoCodeImg from '../picture/women-who-code.png'
 import UxAndUiImg from '../picture/uxanduidesign.jpeg'
 import WebDevImg from '../picture/software developer.jpeg'
@@ -19,7 +20,7 @@ export default class BlogsAtHomePage extends Component {
                 <div className='blogs-container'>
                     <div className='blog-card'>
                         <div className='img-container'>
-                            <img src={WomenWhoCodeImg} alt='women who code' className='blog-img'/>
+                            <img src={WomenWhoCodeImg} alt='women who code' className='blog-homepage-img'/>
                         </div>
                         <div className='blog-link-container'>
                             <a href='https://alicerichardson28.medium.com/my-experience-as-a-speaker-for-women-who-code-6beb99ae0de5' target="_blank" rel="noopener noreferrer">
@@ -29,7 +30,7 @@ export default class BlogsAtHomePage extends Component {
                     </div>
                     <div className='blog-card'>
                         <div className='img-container'>
-                            <img src={UxAndUiImg} alt='ux and ui design' className='blog-img'/>
+                            <img src={UxAndUiImg} alt='ux and ui design' className='blog-homepage-img'/>
                         </div>
                         <div className='blog-link-container'>
                             <a href='https://alicerichardson28.medium.com/as-a-frontend-developer-should-you-know-about-ui-and-ux-8e78c7a955ee' target="_blank" rel="noopener noreferrer">
@@ -39,7 +40,7 @@ export default class BlogsAtHomePage extends Component {
                     </div>
                     <div className='blog-card'>
                         <div className='img-container'>
-                            <img src={WebDevImg} alt='web developer' className='blog-img'/>
+                            <img src={WebDevImg} alt='web developer' className='blog-homepage-img'/>
                         </div>
                         <div className='blog-link-container'>
                             <a href='https://alicerichardson28.medium.com/how-i-become-a-software-developer-with-english-as-my-second-language-937a516e3530' target="_blank" rel="noopener noreferrer">
